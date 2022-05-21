2af135eaf81a5c455dee8e1198fa82ceb6085baa
HanEunHyuk <changehan123@gmail.com>
2022-05-21 13:52:19 +0900

    MODIFY voice.py file

--- control/voice.py
+++ control/voice.py
@@ -7,8 +7,10 @@ with sr.Microphone() as source:
     print('Speak Anything')
     audio = r.listen(source)
 
-    try:
-        text = r.recognize_google(audio,language='ko-KR,en-US')
-        print('당신이 한 말 : {}'.format(text))
-    except:
-        print('죄송합니다. 알아듣지 못했습니다.')
+try:
+    transcript=r.recognize_google(audio, language="ko-KR, en-US") # ko-KR, en-US
+    print("당신이 했던 말 : "+transcript)
+except sr.UnknownValueError:
+    print("당신이 한말을 이해하지 못하였습니다.")
+except sr.RequestError as e:
+    print("당신이 한말을 불러올 수 없습니다.; {0}".format(e))
